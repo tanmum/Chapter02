46630c3db3a3cc0fe792255ff7ec5cb5661618dc
Tan Bui <tanbui@Tans-MacBook-Pro-2.local>
2014-04-01 23:36:18 -0500

    Initial Commit

---
+++ Lab 2.1/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Lab 2.1
+//
+//  Created by Tan Bui on 4/1/14.
+//  Copyright (c) 2014 tanbui. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
